db0575fd4840b5e8f8c873b29d3932f14b5da3df
Jared Williams <jdwux80@gmail.com>
2019-04-24 16:05:31 -0400

    Firebase initializer added to js file.

---
+++ assets/javascript/brewdew.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDwANnYp-1wg5MrzxtdQ0tKD35DPGRpsCg",
+    authDomain: "brewdew-app.firebaseapp.com",
+    databaseURL: "https://brewdew-app.firebaseio.com",
+    projectId: "brewdew-app",
+    storageBucket: "brewdew-app.appspot.com",
+    messagingSenderId: "944789024698"
+  };
+  firebase.initializeApp(config);
